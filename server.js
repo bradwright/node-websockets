95a00a88118fc5935ddc25a4111f7b02f910a34d
Bradley Wright <brad@intranation.com>
2011-06-12 00:28:36 +0100

    Fully working WebSockets server for hybrid-00 version

--- server.js
+++ server.js
@@ -6,7 +6,71 @@
 // Use strict compilation rules - we're not animals
 'use strict';
 
-var net = require('net');
+var net = require('net'),
+    crypto = require('crypto');
+
+function bigEndian(value) {
+    var result = [
+        String.fromCharCode(value >> 24 & 0xFF),
+        String.fromCharCode(value >> 16 & 0xFF),
+        String.fromCharCode(value >> 8 & 0xFF),
+        String.fromCharCode(value & 0xFF)
+    ];
+    return result.join('');
+}
+
+function computeKey(key) {
+    /*
+     * For each of these fields, the server has to
+     * take the digits from the value to obtain a
+     * number, then divide that number by the number
+     * of spaces characters in the value to obtain
+     * a 32-bit number.
+     */
+    var length = parseInt(key.match(/\s/g).length),
+        chars = parseInt(key.replace(/[^0-9]/g, ''));
+    return (chars / length);
+}
+
+// protocol 00
+// http://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-00
+function HandshakeHYBI00(request) {
+    // split up lines and parse
+    var lines = request.split('\r\n'),
+        headers = parseHeaders(lines);
+    if ('sec-websocket-key1' in headers &&
+        'sec-websocket-key2' in headers) {
+        var key1 = computeKey(headers['sec-websocket-key1']),
+            key2 = computeKey(headers['sec-websocket-key2']),
+            /*
+             * The third piece of information is given
+             * after the fields, in the last eight
+             * bytes of the handshake, expressed here
+             * as they would be seen if interpreted as ASCII
+             */
+            data = request.slice(-8, request.length),
+            // md5 hhash
+            hash = crypto.createHash('md5');
+        // update hash with all values
+        hash.update(bigEndian(key1));
+        hash.update(bigEndian(key2));
+        hash.update(data);
+        // TODO: make this a template maybe
+        var response = [
+            'HTTP/1.1 101 WebSocket Protocol Handshake',
+            'Upgrade: WebSocket',
+            'Connection: Upgrade',
+            'Sec-WebSocket-Origin: ' + headers['origin'],
+            // TODO: ws or wss
+            // TODO: add actual request path
+            'Sec-WebSocket-Location: ws://' + headers['host'] + '/',
+            '',
+            hash.digest('binary')
+        ];
+        return response;
+    }
+    return false;
+}
 
 function parseHeaders(headers) {
     // splits a list of headers into key/value pairs
@@ -14,12 +78,13 @@ function parseHeaders(headers) {
 
     headers.forEach(function(header) {
         // might contain a colon, so limit split
-        var toParse = header.split(':', 2);
-
-        if (toParse.length === 2) {
+        var toParse = header.split(':');
+        if (toParse.length >= 2) {
             // it has to be Key: Value
             var key = toParse[0].toLowerCase(),
-                value = toParse[1].replace(/^\s\s*/, '')
+                // might be more than 1 colon
+                value = toParse.slice(1).join(':')
+                    .replace(/^\s\s*/, '')
                     .replace(/\s\s*$/, '');
             parsedHeaders[key] = value;
         }
@@ -35,18 +100,6 @@ function parseHeaders(headers) {
     return parsedHeaders;
 }
 
-// TODO: make this a class that varies based on which
-//       ws protocol is being used
-function validateHandshake(data) {
-    /*
-     * Returns a boolean for whether the handshake succeeded or not
-     */
-    var lines = data.split('\r\n'),
-        headers = parseHeaders(lines);
-    console.log(headers);
-    return false;
-}
-
 var WebsocketServer = net.createServer(function (socket) {
     // listen for connections
     var wsConnected = false;
@@ -57,8 +110,16 @@ var WebsocketServer = net.createServer(function (socket) {
             console.log(data.toString('utf8'));
         }
         else {
-            if (validateHandshake(data.toString('binary'))) {
+            var response = HandshakeHYBI00(data.toString('binary'));
+            if (response) {
+                // handshake succeeded, open connection
+                socket.write(response.join('\r\n'), 'binary');
                 wsConnected = true;
+            }
+            else {
+                // close connection, handshake bad
+                socket.end();
+                return;
             }
         }
     });
