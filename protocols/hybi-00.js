8df184a9cc4070bb9ebb09bae01eca756d4c71d8
Bradley Wright <brad@intranation.com>
2011-06-19 22:26:07 +0100

    Very rough draft of the API I want to move to

--- protocols/hybi-00.js
+++ protocols/hybi-00.js
@@ -42,6 +42,34 @@ function ProtocolHybi00(parsedRequest, data) {
     }
     return false;
 
+    return {
+        parseMessage: function(data) {
+            // this is for protocol 00
+            // text-frame    = %x00 *( UTF8-char ) %xFF
+            var chunks = data.split('');
+            if (chunks[0] == '\u0000' &&
+                chunks[data.length - 1] == '\uffff') {
+                return data.slice(1, data.length - 1);
+            }
+            // framing was broken, we don't want to parse this
+            // TODO: should we abort the message at this point?
+            return false;
+        },
+        sendMessage: function(message) {
+            // this is for protocol 00
+            // text-frame    = %x00 *( UTF8-char ) %xFF
+            this.socket.write('\u0000', 'binary');
+            this.socket.write(message, 'utf8');
+            this.socket.write('\uffff', 'binary');
+            return true;
+        },
+        disconnect: function() {
+            // Send a 0xFF byte and a 0x00 byte to the client
+            // to indicate the start of the closing handshake.
+            this.socket.write('\uffff', 'binary');
+            this.socket.end('\u0000','binary');
+        }
+    };
 };
 
 module.exports = ProtocolHybi00;
